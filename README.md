5a23cf0ecc3daf9eadb4caa336bb6fd434211985
Simon Palmqvist <simon.palmqvist@gmail.com>
2016-01-23 12:24:55 +0100

    Adding nodemon and watchers for json and hbs files

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Email Template Builder
 
 More information to come...
+
+
+##Build and test scripts
+
+###Build a template
+FILE=./example/example.json npm run build:template
+
+###Watch a template
--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 "use strict";
 
 const htmlGen = require("./lib/htmlGenerator");
+const fs = require("fs");
 
-const exampleData = require("./example/example.json");
+if (process.argv.length > 2) {
+    let html = htmlGen.generateTemplate(require(process.argv[2]));
+    fs.writeFile("./example/example.html", html, (error) => {
+        if (error)  {
+            throw error;
+        }
 
-let html = htmlGen.generateTemplate(exampleData);
-
-console.log(html);
+        console.log("updated file");
+    });
+}
