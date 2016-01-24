a08e24ed6632e1d24d5cdc1d3cbf217b7bf30730
Simon Palmqvist <simon.palmqvist@gmail.com>
2016-01-24 12:43:04 +0100

    Adding inheritance of settings for children object. Adding text specifics and images. Refactoring hbs partials and helpers.

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ const htmlGen = require("./lib/htmlGenerator");
 const fs = require("fs");
 
 if (process.argv.length > 2) {
+    console.time("Total");
+    console.time("Generation");
     let html = htmlGen.generateTemplate(require(process.argv[2]));
+    console.timeEnd("Generation");
     fs.writeFile("./example/example.html", html, (error) => {
         if (error)  {
             throw error;
         }
 
+        console.timeEnd("Total");
         console.log("updated file");
     });
 }
